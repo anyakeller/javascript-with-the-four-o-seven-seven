973b15bfb323e8c9a31d4f0f1fb4be4ac5f12278
Anya L Keller <kelleranyal@gmail.com>
2020-08-23 10:51:42 -0400

    woah

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(DIST_DIR));
 
